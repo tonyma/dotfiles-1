4df10e34a45eba3b104d9f9bc1c9375aa3e55b78
Kyoichiro Yamada <me@kyoh86.dev>
2021-02-10 10:25:15 +0900

    fix

--- nvim/lua/my-galaxyline.lua
+++ nvim/lua/my-galaxyline.lua
@@ -1,47 +1,144 @@
 local gl = require('galaxyline')
+local provider_vcs = require('galaxyline.provider_vcs')
 local gls = gl.section
 
-local M = {
-  mode_texts = {
-    n      = "\u{E7C5}  ",
-    no     = "\u{E7C5}  ",
-    i      = "\u{FFAE6}  ",
-    ic     = "\u{FFAE6}  ",
-    r      = "\u{FF954}  ",
-    rm     = "\u{FF954}  ",
-    ['r?'] = "\u{FF954}  ",
-    v      = "\u{FF761}  ",
-    [''] = "\u{FF767}  ",
-    V      = "\u{FF760}  ",
-    s      = "\u{FFB51}  ",
-    S      = "\u{FFB51}  ",
-    ['^S'] = "\u{FFB51}  ",
-    R      = "\u{FF954}  ",
-    Rv     = "\u{FF954}  ",
-    c      = "： ",
-    cv     = "： ",
-    ce     = "： ",
-    t      = "\u{F120}  ",
-    ['!']  = "\u{F120}  ",
-  },
-  mode_colors = {},
+local modeTexts = {
+  n      = "\u{E7C5}  ",
+  no     = "\u{E7C5}  ",
+  i      = "\u{FFAE6}  ",
+  ic     = "\u{FFAE6}  ",
+  r      = "\u{FF954}  ",
+  rm     = "\u{FF954}  ",
+  ['r?'] = "\u{FF954}  ",
+  v      = "\u{FF761}  ",
+  [''] = "\u{FF767}  ",
+  V      = "\u{FF760}  ",
+  s      = "\u{FFB51}  ",
+  S      = "\u{FFB51}  ",
+  ['^S'] = "\u{FFB51}  ",
+  R      = "\u{FF954}  ",
+  Rv     = "\u{FF954}  ",
+  c      = "： ",
+  cv     = "： ",
+  ce     = "： ",
+  t      = "\u{F120}  ",
+  ['!']  = "\u{F120}  ",
 }
 
-M.get_mode_color = function()
-  -- モードに応じた色のセット {deep = ..., light = ...} を返す
-  local mode_color = M.mode_colors[vim.fn.mode()]
-  local file_type = vim.api.nvim_buf_get_option(0, 'filetype')
-  if file_type == 'fern' then
-    local fern_mode = vim.b.my_fern_mode
-    if fern_mode == 'operate' then
-      mode_color = {palette.magenta, palette.lightmagenta}
+local modeColors = {}
+local palette = {}
+
+-- モードを表示してモードに応じたHighlightを設定するprovider
+local function modeProvider()
+  -- 色のセット {deep = ..., light = ...} を得る
+  local modeColor = modeColors[vim.fn.mode()]
+  local fileType = vim.api.nvim_buf_get_option(0, 'filetype')
+  if fileType == 'fern' then
+    local fernMode = vim.b.my_fern_mode
+    if fernMode == 'operate' then
+      modeColor = {palette.magenta, palette.lightmagenta}
     end
   end
-  return mode_color
+
+  -- highlightを用意する
+  vim.cmd('hi GalaxyDeep     guifg=' .. palette.black  .. ' guibg=' .. modeColor.deep)
+  vim.cmd('hi GalaxyDeepSep  guifg=' .. modeColor.deep .. ' guibg=' .. modeColor.light)
+  vim.cmd('hi GalaxyLight    guifg=' .. palette.black  .. ' guibg=' .. modeColor.light)
+  vim.cmd('hi GalaxyLightSep guifg=' .. palette.black  .. ' guibg=' .. modeColor.light)
+
+  return "\u{00A0}" .. modeTexts[vim.fn.mode()]
 end
 
-M.setup = function(palette)
-  M.mode_colors = {
+-- 空文字を返すprovider
+local function emptyProvider() return '' end
+
+-- Gitのディレクトリにいるかどうか
+local function isinGitDir()
+  local cwd = vim.fn.getcwd()
+  local result = provider_vcs.get_git_dir(cwd)
+  if not result then
+    return false
+  end
+  return true
+end
+
+-- 現在のディレクトリ名を得るprovider
+local function cwdProvider ()
+  return vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+end
+
+-- 現在のディレクトリのGit Branchを取得するProvider
+function gitBranchProvider()
+  local cwd = vim.fn.getcwd()
+  local gitDir = provider_vcs.get_git_dir(cwd)
+  if not gitDir then return end
+
+  -- If git directory not found then we're probably outside of repo or
+  -- something went wrong. The same is when headFile is nil
+  local headFile = io.open(gitDir..'/HEAD')
+  if not headFile then return end
+
+  local HEAD = headFile:read()
+  headFile:close()
+
+  -- If HEAD matches branch expression, then we're on named branch
+  -- otherwise it is a detached commit
+  local branchName = HEAD:match('ref: refs/heads/(.+)')
+  if branchName == nil then return  end
+
+  return branchName .. ' '
+end
+--
+-- バッファがファイルを開いているかどうか
+local function bufferHasFile()
+  if vim.fn.empty(vim.fn.expand('%:t')) ~= 1 then
+    return true
+  end
+  return false
+end
+
+-- 現在のディレクトリのGit Statusを取得するProvider
+local function gitStatProvider()
+  if vim.bo.buftype == 'terminal' then
+    return '' -- TODO: get title and parse it as path
+  end
+
+  local location = vim.fn.getcwd()
+  local stat = require('my-gitstate').get_git_status(location)
+  if stat == nil then
+    return ''
+  end
+
+  local function withPrefix(prefix, value)
+    if not value or value == 0 then
+      return ''
+    end
+    return prefix .. ' ' .. value
+  end
+
+  local output = vim.fn.trim(vim.fn.join(vim.tbl_filter(function(w)
+    return w ~= nil and w ~= ''
+  end, {
+    withPrefix("\u{FF55D}", stat.ahead),     -- 󿕝 .
+    withPrefix("\u{FF545}", stat.behind),    -- 󿕅 .
+    stat.sync,
+    withPrefix('\u{FFBC2}', stat.unmerged),  -- 󿯂 .
+    withPrefix("\u{FF62B}", stat.staged),    -- 󿘫 .
+    withPrefix("\u{FF914}", stat.unstaged),  -- 󿤔 .
+    withPrefix("\u{FF7D5}", stat.untracked), -- 󿟕 .
+  })))
+  if output == '' then
+    return output
+  else
+    return ' ' .. output .. ' '
+  end
+end
+
+local M = {}
+
+M.setup = function(newPalette)
+  palette = newPalette
+  modeColors = {
     n      = {deep = palette.green,   light = palette.lightgreen},
     no     = {deep = palette.green,   light = palette.lightgreen},
     i      = {deep = palette.blue,    light = palette.lightblue},
@@ -64,160 +161,122 @@ M.setup = function(palette)
     t      = {deep = palette.red,     light = palette.lightred},
   }
 
-  local function hi(component, fg, bg)
-    -- 対象コンポーネントに色を付ける
-    vim.cmd('hi Galaxy' .. component .. '    guifg=' .. fg ..' guibg=' .. bg)
-  end
+  gls.left = {}
 
-  local function hisep(component, fg, bg)
-    -- 対象コンポーネントのセパレータに色を付ける
-    vim.cmd('hi Galaxy' .. component .. 'Separator guifg=' .. fg ..' guibg=' .. bg)
-  end
+  -- show mode
+  table.insert(gls.left, { ViMode = {
+    provider  = modeProvider,
+    separator = "\u{E0B0}\u{00A0}",
+    highlight           = 'GalaxyDeep',
+    separator_highlight = 'GalaxyDeepSep',
+  }})
 
-  -- 真ん中の領域をモードカラーで埋めるため、左右の一番真ん中よりに置くcomponent
-  local edge = {
-    provider = function() return '' end,
-    highlight = 'GalaxyLight',
-    separator_highlight = 'GalaxyLightSep'
-  }
+  -- show current dir
+  table.insert(gls.left, {
+    CWD = {
+      provider  = cwdProvider,
+      separator = "\u{00A0}\u{E0B1}\u{00A0}",
+      highlight           = 'GalaxyLight',
+      separator_highlight = 'GalaxyLightSep',
+    }
+  })
 
-  gls.left[1] = {
-    ViMode = {
-      provider = function()
-        local mode_color = M.get_mode_color()
-        vim.cmd('hi GalaxyDeep     guifg=' .. palette.black   .. ' guibg=' .. mode_color.deep)
-        vim.cmd('hi GalaxyDeepSep  guifg=' .. mode_color.deep .. ' guibg=' .. mode_color.light)
-        vim.cmd('hi GalaxyLight    guifg=' .. palette.black   .. ' guibg=' .. mode_color.light)
-        vim.cmd('hi GalaxyLightSep guifg=' .. palette.black   .. ' guibg=' .. mode_color.light)
-        return "\u{00A0}" .. M.mode_texts[vim.fn.mode()]
-      end,
-      highlight = 'GalaxyDeep',
+  -- show current branch
+  table.insert(gls.left, {
+    GitBranch = {
+      condition = isinGitDir,
+      icon = '\u{E0A0} ',
+      provider  = gitBranchProvider,
+      separator = "\u{E0B1}\u{00A0}",
+      highlight           = 'GalaxyLight',
+      separator_highlight = 'GalaxyLightSep',
+    }
+  })
 
-      separator = "\u{E0B0}\u{00A0}",
-      separator_highlight = 'GalaxyDeepSep',
-    },
-  }
-
-  gls.left[2] = {
+  -- show current file icon
+  table.insert(gls.left, {
     FileIcon = {
-      condition = function()
-        if vim.fn.empty(vim.fn.expand('%:t')) ~= 1 then
-          return true
-        end
-        return false
-      end,
+      condition = bufferHasFile,
       provider = 'FileIcon',
       highlight = 'GalaxyLight',
 
-      separator = "\u{00A0}\u{E0B1}\u{00A0}",
+      separator = " ",
       separator_highlight = 'GalaxyLightSep',
     },
-  }
+  })
 
-  -- get current dir
-  gls.left[3] = {
-    CWD = {
-      provider = function ()
-        return vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
-      end,
-      highlight = 'GalaxyLight',
-
-      separator = "\u{00A0}\u{E0B1}\u{00A0}",
-      separator_highlight = 'GalaxyLightSep',
-    }
-  }
-
-  local provider_vcs = require('galaxyline.provider_vcs')
-
-  -- get current branch
-  gls.left[4] = {
-    GitBranch = {
-      condition = function()
-        local current_dir = vim.fn.getcwd()
-        local result = provider_vcs.get_git_dir(current_dir)
-        if not result then
-          return false
-        end
-        return true
-      end,
-      icon = '\u{E0A0} ',
-
-      provider = 'GitBranch',
-      highlight = 'GalaxyLight',
-
-      separator = "\u{00A0}\u{E0B1}\u{00A0}",
-      separator_highlight = 'GalaxyLightSep',
-    }
-  }
-
-  -- get current file name
-  gls.left[5] = {
+  -- show current file name
+  table.insert(gls.left, {
     FileName = {
       provider = 'FileName',
       highlight = 'GalaxyLight',
     }
+  })
+
+  table.insert(gls.left, { LeftEdge = {
+    provider = emptyProvider,
+    highlight = 'GalaxyLight',
+  }})
+
+  gls.right = {}
+
+  table.insert(gls.right, { RightEdge = {
+    provider = emptyProvider,
+    highlight = 'GalaxyLight',
+  }})
+
+  -- show git stat
+  table.insert(gls.right, { GitStat = {
+    provider  = gitStatProvider,
+    separator = ' ',
+    highlight           = { palette.lightred, palette.black },
+    separator_highlight = 'GalaxyLightSep'
+  }})
+
+  table.insert(gls.right, { DiagnosticHint  = {
+    provider = 'DiagnosticHint',
+    icon = '\u{F059}'
+  }})
+
+  table.insert(gls.right, { DiagnosticInfo  = {
+    provider = 'DiagnosticInfo',
+    icon = '\u{F05A}'
+  }})
+
+  table.insert(gls.right, { DiagnosticWarn  = {
+    provider = 'DiagnosticWarn',
+    icon = '\u{F06A}'
+  }})
+
+  table.insert(gls.right, { DiagnosticError = {
+    provider = 'DiagnosticError',
+    icon = '\u{F057}'
+  }})
+
+  gls.short_line_left = {
+    { ShortLeftEdge = {
+      provider = emptyProvider,
+      highlight = { palette.lightblack, palette.lightblack }
+    }}
   }
 
-  gls.left[5] = { LeftEdge = edge }
-
-  gls.right[1] = { RightEdge = edge }
-
-  local function with_prefix(prefix, value)
-    if not value or value == 0 then
-      return ''
-    end
-    return prefix .. ' ' .. value
-  end
-
-  gls.right[2] = {
-    GitStat = {
-      provider = function()
-        if vim.bo.buftype == 'terminal' then
-          return '' -- TODO: get title and parse it as path
-        end
-
-        local location = vim.fn.getcwd()
-        local stat = require('my-gitstate').get_git_status(location)
-        if stat == nil then
-          return ''
-        end
-
-        local output = vim.fn.trim(vim.fn.join(vim.tbl_filter(function(w)
-          return w ~= nil and w ~= ''
-        end, {
-          with_prefix("\u{FF55D}", stat.ahead),     -- 󿕝 .
-          with_prefix("\u{FF545}", stat.behind),    -- 󿕅 .
-          stat.sync,
-          with_prefix('\u{FFBC2}', stat.unmerged),  -- 󿯂 .
-          with_prefix("\u{FF62B}", stat.staged),    -- 󿘫 .
-          with_prefix("\u{FF914}", stat.unstaged),  -- 󿤔 .
-          with_prefix("\u{FF7D5}", stat.untracked), -- 󿟕 .
-        })))
-        if output == '' then
-          return output
-        else
-          return ' ' .. output .. ' '
-        end
-      end,
-      highlight = { palette.lightred, palette.black },
-
+  gls.short_line_right = {
+    { ShortRightEdge = {
+      provider = emptyProvider,
+      highlight = { palette.lightblack, palette.lightblack }
+    }},
+    { RightGitStat = {
+      provider  = gitStatProvider,
       separator = ' ',
+      highlight           = { palette.lightred, palette.black },
       separator_highlight = 'GalaxyLightSep'
-    }
+    }}
   }
-
-  gls.right[3] = { DiagnosticHint  = { provider = 'DiagnosticHint',  icon = '\u{F059}' } }
-  gls.right[4] = { DiagnosticInfo  = { provider = 'DiagnosticInfo',  icon = '\u{F05A}' } }
-  gls.right[5] = { DiagnosticWarn  = { provider = 'DiagnosticWarn',  icon = '\u{F06A}' } }
-  gls.right[6] = { DiagnosticError = { provider = 'DiagnosticError', icon = '\u{F057}' } }
 
   vim.api.nvim_set_option('showmode', false)  -- galaxyline で表示するので、vim標準のモード表示は隠す
 
   vim.cmd[[ autocmd DirChanged global lua require("galaxyline").load_galaxyline()]]
   vim.cmd[[ autocmd User MyFernModeChanged lua require("galaxyline").load_galaxyline()]]
-
-  gls.short_line_left = gls.left
-  gls.short_line_right = gls.right
 
   gl.load_galaxyline()
 end
